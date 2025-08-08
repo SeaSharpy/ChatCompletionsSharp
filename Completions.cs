70725f334fb949068f1f16374300ff7de4cc8d91
GroceyLot <waltermorton@icloud.com>
2025-08-08 14:54:56 +0100

    \sd

--- Completions.cs
+++ Completions.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json.Linq;
+
 namespace ChatCompletionSharp;
 public class CompletionRequest
 {
--- OpenAI.cs
+++ OpenAI.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using Newtonsoft.Json.Linq;
 
 namespace ChatCompletionsSharp;
 
